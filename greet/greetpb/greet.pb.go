b1fda08f614e948c6caffbe108ecfb7d4cbb38c7
Avery Wong <avwong13@g.ucla.edu>
2020-02-16 12:28:11 -0800

    finished ch 17

--- greet/greetpb/greet.pb.go
+++ greet/greetpb/greet.pb.go
@@ -4,8 +4,10 @@
 package greetpb
 
 import (
+	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
+	grpc "google.golang.org/grpc"
 	math "math"
 )
 
@@ -29,4 +31,46 @@ var fileDescriptor_32c0044392f32579 = []byte{
 	0x41, 0x8c, 0xe0, 0xd4, 0xa2, 0xb2, 0xcc, 0xe4, 0x54, 0x27, 0xce, 0x28, 0x76, 0xb0, 0x44, 0x41,
 	0x52, 0x12, 0x1b, 0x58, 0xa1, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0x26, 0x07, 0x30, 0x9e, 0x37,
 	0x00, 0x00, 0x00,
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ grpc.ClientConnInterface
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion6
+
+// GreetServiceClient is the client API for GreetService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type GreetServiceClient interface {
+}
+
+type greetServiceClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewGreetServiceClient(cc grpc.ClientConnInterface) GreetServiceClient {
+	return &greetServiceClient{cc}
+}
+
+// GreetServiceServer is the server API for GreetService service.
+type GreetServiceServer interface {
+}
+
+// UnimplementedGreetServiceServer can be embedded to have forward compatible implementations.
+type UnimplementedGreetServiceServer struct {
+}
+
+func RegisterGreetServiceServer(s *grpc.Server, srv GreetServiceServer) {
+	s.RegisterService(&_GreetService_serviceDesc, srv)
+}
+
+var _GreetService_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "greet.GreetService",
+	HandlerType: (*GreetServiceServer)(nil),
+	Methods:     []grpc.MethodDesc{},
+	Streams:     []grpc.StreamDesc{},
+	Metadata:    "greet.proto",
 }
